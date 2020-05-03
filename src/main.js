ff7a19186aa36e36c728e74f2eda0b48f4146352
shojay <297876668@qq.com>
2020-05-03 18:32:07 +0800

    login finished

--- src/main.js
+++ src/main.js
@@ -1,9 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import axios from 'axios'
 import './plugins/element.js'
+import './assets/css/global.css'
+import './assets/font/iconfont.css'
 
 Vue.config.productionTip = false
+
+Vue.prototype.$axios = axios
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
 
 new Vue({
   router,
